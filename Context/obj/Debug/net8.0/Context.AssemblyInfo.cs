4407dc1229cca1fc70c8544af8b3fcfc44c52cef
yasserabdelfadile <yasserabdelfadile.tech@gmail.com>
2024-08-31 21:19:31 +0300

    git last

--- Context/obj/Debug/net8.0/Context.AssemblyInfo.cs
+++ Context/obj/Debug/net8.0/Context.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Context")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf2c16be1a1df6f9e8258ce36a16440fd7a593c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1681b4c27180aad79a66b5f9f3473a745976d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Context")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Context")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
