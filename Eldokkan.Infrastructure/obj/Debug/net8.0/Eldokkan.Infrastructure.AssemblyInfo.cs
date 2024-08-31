ca1681b4c27180aad79a66b5f9f3473a745976d3
yasserabdelfadile <yasserabdelfadile.tech@gmail.com>
2024-08-31 21:02:44 +0300

    resolve issue

--- Eldokkan.Infrastructure/obj/Debug/net8.0/Eldokkan.Infrastructure.AssemblyInfo.cs
+++ Eldokkan.Infrastructure/obj/Debug/net8.0/Eldokkan.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eldokkan.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dab2b41f578cd394664d1fbd7b089ac810c193c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf2c16be1a1df6f9e8258ce36a16440fd7a593c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eldokkan.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eldokkan.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
